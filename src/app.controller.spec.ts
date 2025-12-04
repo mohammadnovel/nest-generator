da41adc4d78fe8ac47c5794335b706aabe333c1b
mohramdhani <velrest48@gmail.com>
2025-12-04 18:42:33 +0700

    testcase all pased

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -8,15 +9,25 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: CACHE_MANAGER,
+          useValue: {
+            get: jest.fn(),
+            set: jest.fn(),
+            del: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Bisa di akses ya!"', () => {
+      expect(appController.getHello()).toBe('Bisa di akses ya!');
     });
   });
 });
